37e21e423fb56649e72d6673ecc93d328f5f68ef
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-06-30 13:56:50 +0000

    Understand useState and basic event handling.

--- ReactJs/react-practice/Day11/Problem1.jsx
+++ ReactJs/react-practice/Day11/Problem1.jsx
@@ -8,3 +8,25 @@
 // When "Increment" is clicked, increase the count by 1.
 // When "Decrement" is clicked, decrease the count by 1.
 // Concepts Covered: Functional Components, JSX, useState, Event Handling (onClick).
+
+
+import React, {useState} from 'react';
+function Counter() {
+    const [count, setCount]=useState(0);
+     const increment = () => setCount(count + 1);
+     const decrement = () => setCount(count - 1);
+
+     return(
+        <div>
+            <h2>
+                Simple Counter
+            </h2>
+            <p>
+                Count: {count} 
+            </p>
+            <button onClick={increment}> Increment</button>
+             <button onClick={decrement}> Decrement</button>
+        </div>
+     );
+} 
+export default Counter;
