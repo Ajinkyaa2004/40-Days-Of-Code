359b4ef54a0b495ac5ec1421c9f80c6b33e79af5
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-07-10 08:03:08 +0000

    Understand controlled components for forms and basic useEffect usage.

---
+++ ReactJs/react-practice/Day20/Problem10.jsx
@@ -0,0 +1,11 @@
+// Problem 4: Character Counter (Input Handling and useEffect - Basic)
+// Goal: Understand controlled components for forms and basic useEffect usage.
+// Tasks:
+// Create a component CharacterCounter.
+// Use useState to manage the text entered in a <textarea>.
+// Display the current number of characters typed below the textarea.
+// Use useEffect to log the character count to the console every time the text changes.
+// Concepts Covered: Controlled Components (textarea), useState, useEffect (with dependency array), Event Handling (onChange).
+
+
+
