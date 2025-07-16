b05989330974dbf516a41c7ca4f79a8755c87c6f
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-07-16 17:38:24 +0000

    Work with one-dimensional arrays and loops.

--- Java/Day3.java
+++ Java/Day3.java
@@ -10,3 +10,45 @@
 // Concepts Covered: Array declaration and initialization, array indexing, for loop, for-each loop (enhanced for loop).
 
 
+public class ArrayOperations {
+    public static void main(String[] args) {
+        int[] numbers = {10, 25, 5, 40, 15, 30};
+
+        // 🔹 Find Max
+        int max = numbers[0];
+        for (int num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.println("Maximum number: " + max);
+
+        // 🔹 Sum All
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        System.out.println("Sum of all numbers: " + sum);
+
+        // 🔹 Print Even
+        System.out.print("Even numbers: ");
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+
+        // 🔹 Reverse Array
+        int[] reversed = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            reversed[i] = numbers[numbers.length - 1 - i];
+        }
+
+        System.out.print("Reversed array: ");
+        for (int num : reversed) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
