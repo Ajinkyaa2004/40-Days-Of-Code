b698c7957fdaf86fa5f99c0f245e899fd30459b4
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-07-16 17:34:29 +0000

    Work with one-dimensional arrays and loops.

---
+++ Java/Day3.java
@@ -0,0 +1,10 @@
+Problem 3: Array Operations (Arrays, Loops)
+Goal: Work with one-dimensional arrays and loops.
+Tasks:
+Create a class ArrayOperations.
+In the main method, declare and initialize an integer array, e.g., int[] numbers = {10, 25, 5, 40, 15, 30};.
+Find Max: Write a loop to find and print the largest number in the array.
+Sum All: Write a loop to calculate and print the sum of all numbers in the array.
+Print Even: Write a loop to print only the even numbers from the array.
+Reverse Array: Create a new array and fill it with the elements of the numbers array in reverse order. Print the reversed array.
+Concepts Covered: Array declaration and initialization, array indexing, for loop, for-each loop (enhanced for loop).
