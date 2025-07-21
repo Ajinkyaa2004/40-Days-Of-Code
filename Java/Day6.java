ae0757330933fc66df5368a4d4f2bef771947fd3
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-07-21 15:35:36 +0000

    Demonstrate polymorphism using a common superclass reference.

--- Java/Day6.java
+++ Java/Day6.java
@@ -8,3 +8,40 @@
 // Use a for-each loop to iterate through the shapes array.
 // Inside the loop, call calculateArea() and display() on each Shape object. Observe how the correct calculateArea() method (from the actual object type) is invoked.
 // Concepts Covered: Polymorphism (runtime), Array of objects, for-each loop.
+
+public class PolymorphismDemo {
+
+
+    static class Animal {
+        public void makeSound() {
+            System.out.println("Animal makes a sound");
+        }
+    }
+
+
+    static class Dog extends Animal {
+        @Override
+        public void makeSound() {
+            System.out.println("Dog barks");
+        }
+    }
+
+
+    static class Cat extends Animal {
+        @Override
+        public void makeSound() {
+            System.out.println("Cat meows");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Animal myAnimal;
+
+        myAnimal = new Dog();  
+        myAnimal.makeSound();   
+
+        myAnimal = new Cat();  
+        myAnimal.makeSound();   
+    }
+}
