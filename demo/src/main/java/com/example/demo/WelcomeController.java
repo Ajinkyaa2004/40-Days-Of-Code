cac5ca3c43ff5bfe0e90d4fc7bd66c8477ed0b77
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-07-29 16:46:53 +0000

    Part 4: SpringBoot Fundamentals (10 Problems)

---
+++ demo/src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public Map<String, String> welcome() {
+        return Map.of("message", "Welcome to Spring Boot!");
+    }
+}
