2fb190a80631d830831820665d1f9416a6999860
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-07-29 16:47:42 +0000

    Understand project setup, @RestController, and basic @GetMapping.

--- demo/src/main/java/com/example/demo/WelcomeController.java
+++ demo/src/main/java/com/example/demo/WelcomeController.java
@@ -1,3 +1,9 @@
+// 1. Simple REST API
+// Problem:Create a basic Spring Boot application that exposes a GET API at /welcome, returning a welcome message in JSON format.
+// Goal:Understand project setup, @RestController, and basic @GetMapping.
+
+
+
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
