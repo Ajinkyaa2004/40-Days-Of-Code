6868d983debf0ab3331cc0fb81e85aedc54947fb
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-08-03 15:07:15 +0000

    Global Exception Handling

---
+++ demo/src/main/java/com/DemoApplication1.java
@@ -0,0 +1,3 @@
+// 4. Global Exception Handling
+// Problem:Create a centralized exception handler using @ControllerAdvice to catch and format errors from any controller.
+// Goal:Learn exception handling using @ExceptionHandler and build consistent error responses.
